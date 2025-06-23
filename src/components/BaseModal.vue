70f09e6bfe7a30e9f1de25acb9baf42e8d84fdda
natthachai.s <natthachaisae@gosoft.co.th>
2025-06-23 11:57:52 +0700

    feat: adjust BaseModal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -4,7 +4,6 @@ import { useBookStore } from '../store/book'
 const bookStore = useBookStore()
 
 const validateForm = () => {
-    console.log('formState.value :>> ', bookStore.formState);
     const { title, author, published_year, genre } = bookStore.formState
     const errors = []
 
@@ -28,8 +27,8 @@ const handleSubmit = () => {
 
 <template>
     <div v-if="bookStore.showModal"
-        class="fixed inset-0 z-50 bg-gray-400/80 transition-opacity flex items-center justify-center">
-        <div class="bg-white rounded-xl w-full max-w-lg p-6 shadow-lg">
+        class="fixed inset-0 z-50 bg-gray-400/40 transition-opacity flex items-center justify-center">
+        <div class="bg-white rounded-xl w-full max-w-lg p-6 shadow-lg m-2">
             <h2 class="text-lg font-semibold mb-4">
                 {{ bookStore.isEditMode ? 'Edit Book' : 'Add Book' }}
             </h2>
@@ -38,13 +37,13 @@ const handleSubmit = () => {
 
             <!-- actions -->
             <div class="pt-4 justify-end sm:flex sm:flex-row">
-                <button type="submit" @click="handleSubmit"
-                    class="cursor-pointer inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-600/80 sm:mr-3 sm:w-auto">
-                    Confirm
-                </button>
                 <button type="button" @click="bookStore.closeModal"
-                    class="cursor-pointer mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 sm:mt-0 sm:w-auto">
+                    class="cursor-pointer mt-3 inline-flex w-full justify-center rounded-md bg-gray-50 px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 my-3 sm:my-0 sm:w-auto sm:mr-3 ">
                     Cancel
+                </button>
+                <button type="submit" @click="handleSubmit"
+                    class="cursor-pointer inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-600/80 sm:w-auto">
+                    Confirm
                 </button>
             </div>
         </div>
